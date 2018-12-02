2929dc53c0af2feb41a80e82186f188c5d8dc5a0
Suele <suelen.ads.guimaraes@gmail.com>
2018-12-02 11:19:14 -0200

    Adiciona a importação da rota.

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,4 +1,8 @@
+require('marko/node-require').install();// para o node
+require('marko/express');// para o express
 const express = require('express');
 const app = express();
+const router = require('../app/router/router');// importação da rota.
+router(app);
 
 module.exports = app;
