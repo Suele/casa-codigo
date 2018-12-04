9fb4c803e19ab7dc0524db91b0332bf7a8ecdabe
Suele <suelen.ads.guimaraes@gmail.com>
2018-12-04 10:21:20 -0200

    Adiciona o middeler body-parse.

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,8 +1,13 @@
 require('marko/node-require').install();// para o node
 require('marko/express');// para o express
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const router = require('../app/router/router');// importação da rota.
+
+app.use(bodyParser.urlencoded({ extended:true }));
+
 router(app);
 
 module.exports = app;
