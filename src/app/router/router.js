c1bb6e722011dbbebdd1879a300090afdbd1f940
Suele <suelen.ads.guimaraes@gmail.com>
2018-12-04 10:23:43 -0200

    Adiciona a rota do formulario de livros.

---
+++ src/app/router/router.js
@@ -0,0 +1,32 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
+module.exports = (app) => {
+    app.get('/livros', (req, res) => {
+        const livroDao = new LivroDao(db);
+        
+        livroDao.lista()
+            .then(livrosSelecionados => {
+                res.marko(
+                    require('../views/livros/lista/lista.marko'), {
+                        livros: livrosSelecionados
+                    }
+                );
+            }).catch('Nenhum livro foi selecionado!');
+    });
+
+    app.get('/livros/form', (req, res) => {
+        res.marko(
+            require(
+                '../views/livros/form/form.marko'));
+    });
+
+    app.post('/livros', function(req, resp) {
+
+        const livroDao = new LivroDao(db);
+ 
+        livroDao.adiciona(req.body)
+            .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro));
+    });
+};
